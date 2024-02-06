50cad8c47d882699d539f24af11e54afde28fc15
Vansh Malhotra <109238503+Vansh2727@users.noreply.github.com>
2024-02-06 11:01:40 -0700

    v

---
+++ app/week-3/item.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="bg-slate-800 list-none max-w-96  p-4 my-2 rounded-md shadow-md">
+      <h3 className="text-xl font-semibold mb-2">{name}</h3>
+      <p className="text-white">Quantity: {quantity}</p>
+      <p className="text-white">Category: {category}</p>
+    </li>
+  );
+};
+
+export default Item;
