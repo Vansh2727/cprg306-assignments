8a4613e479b01ce60b5f077479f62bf659ab9b36
Vansh Malhotra <109238503+Vansh2727@users.noreply.github.com>
2024-02-06 11:24:32 -0700

    vv

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -4,8 +4,9 @@ const Item = ({ name, quantity, category }) => {
   return (
     <li className="bg-slate-800 list-none max-w-96  p-4 my-2 rounded-md shadow-md">
       <h3 className="text-xl font-semibold mb-2">{name}</h3>
-      <p className="text-white">Quantity: {quantity}</p>
-      <p className="text-white">Category: {category}</p>
+      <p className="text-white">
+        Buy {quantity} in {category}
+      </p>
     </li>
   );
 };
