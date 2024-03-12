b6a31c1205bec1eab53c08b637be1a40b343701d
Vansh Malhotra <109238503+Vansh2727@users.noreply.github.com>
2024-03-12 01:09:49 -0600

    final commit

--- app/page.js
+++ app/page.js
@@ -13,6 +13,8 @@ export default function Home() {
       <Link href="/week-5">week 5</Link>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-6">week 6</Link>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-7">week 7</Link>
     </>
   );
 }
